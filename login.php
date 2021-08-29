2959dac4155fe5b7330d724f9b2781713d2ffe53
Junior Paixao <juniortidesenvolvimento1@gmail.com>
2021-08-28 21:17:31 -0300

    28-08-2021 21:17

---
+++ login.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<meta charset="utf-8">
+	<title>Login Facebook</title>
+	<link rel="stylesheet" href="css/style.css">
+
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Mulish:wght@900&family=Roboto&display=swap" rel="stylesheet"> 
+
+</head>
+<body>
+
+	<main>
+		<div class="main--container">
+			<div class="main--description">
+				<h1>facebook</h1>
+				<p>O Facebook ajuda você a se conectar e compartilhar com as pessoas que fazem parte da sua vida.</p>
+			</div>
+			<div class="main--login">
+				<div class="main--card">
+					<form>
+						<div>
+							<input type="email" name="email" placeholder="Email ou telefone">
+						</div>
+
+						<div>
+							<input type="password" name="password" placeholder="Senha">
+						</div>
+
+						<button type="submit">Entrar</button>
+
+						<div class="main--link">
+							<a href="#">Esqueçeu a senha?</a>
+						</div>
+
+						<hr>
+
+						<div class="main--btn">
+							<a href="#">Criar nova conta</a>
+						</div>
+					</form>
+				</div>
+				<div class="main--footer">
+					<a href="#">Criar uma Página</a> para uma celebridade, banda ou empresa.
+				</div>
+			</div>
+		</div>
+	</main>
+
+	<footer>
+		...
+	</footer>
+
+</body>
+</html>
